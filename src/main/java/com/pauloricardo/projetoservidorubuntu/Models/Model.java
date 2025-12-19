ffb54406dbe93e29720809dd0dfedcb0a4b47fb0
PauloR2021 <paulotrindadeoficial@gmail.com>
2025-12-19 14:10:30 -0300

    Criando o modelo de tabela do banco de dados

---
+++ src/main/java/com/pauloricardo/projetoservidorubuntu/Models/Model.java
@@ -0,0 +1,19 @@
+package com.pauloricardo.projetoservidorubuntu.Models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "produtcs")
+@Getter @Setter
+public class Model {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Integer quantity;
+    private Double price;
+}
