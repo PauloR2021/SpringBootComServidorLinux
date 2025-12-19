436577b46f49120ddf716d918d4a4557c1466ff5
PauloR2021 <paulotrindadeoficial@gmail.com>
2025-12-19 14:22:33 -0300

    Criando as Classes de Exception do Projeto

---
+++ src/main/java/com/pauloricardo/projetoservidorubuntu/Exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.pauloricardo.projetoservidorubuntu.Exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundExeption.class)
+    public ResponseEntity<String> handleNotFound(ResourceNotFoundExeption ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+}
