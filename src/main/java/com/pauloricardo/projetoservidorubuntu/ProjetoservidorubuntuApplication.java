786e9073d6edd24ee1cc3a376d4ce3f2c16e9073
PauloR2021 <paulotrindadeoficial@gmail.com>
2025-12-19 13:18:16 -0300

    Adicionando a Estutura do Projeto

---
+++ src/main/java/com/pauloricardo/projetoservidorubuntu/ProjetoservidorubuntuApplication.java
@@ -0,0 +1,13 @@
+package com.pauloricardo.projetoservidorubuntu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoservidorubuntuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoservidorubuntuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pauloricardo/projetoservidorubuntu/ProjetoservidorubuntuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pauloricardo.projetoservidorubuntu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoservidorubuntuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
