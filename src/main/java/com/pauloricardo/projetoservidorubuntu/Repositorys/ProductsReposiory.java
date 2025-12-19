b0e2d20ba0d960827c0422ed5f17449a94ef481d
PauloR2021 <paulotrindadeoficial@gmail.com>
2025-12-19 14:18:39 -0300

    Criando o Repository do projeto

--- src/main/java/com/pauloricardo/projetoservidorubuntu/Repositorys/ProductsReposiory.java
+++ src/main/java/com/pauloricardo/projetoservidorubuntu/Repositorys/ProductsReposiory.java
@@ -1,4 +1,7 @@
 package com.pauloricardo.projetoservidorubuntu.Repositorys;
 
-public interface ProductsReposiory {
+import com.pauloricardo.projetoservidorubuntu.Models.ProductsModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductsReposiory extends JpaRepository<ProductsModel, Long> {
 }
