d7de3fd12c4cf9c6a1c4549507043cabf2be1ac4
PauloR2021 <paulotrindadeoficial@gmail.com>
2025-12-19 17:14:15 -0300

    Atualizando o POM, Services,Properties e a classe principal

--- src/main/java/com/pauloricardo/projetoservidorubuntu/ProjetoservidorubuntuApplication.java
+++ src/main/java/com/pauloricardo/projetoservidorubuntu/ProjetoservidorubuntuApplication.java
@@ -1,5 +1,6 @@
 package com.pauloricardo.projetoservidorubuntu;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjetoservidorubuntuApplication {
 
 	public static void main(String[] args) {
+
+        // Carrega o .env
+        Dotenv dotenv = Dotenv.configure()
+                .filename(".env.test") // nome do seu arquivo
+                .load();;
+
+        // Define variáveis de ambiente no sistema (opcional)
+        System.setProperty("SPRING_PROFILE", dotenv.get("SPRING_PROFILE"));
+        System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(ProjetoservidorubuntuApplication.class, args);
 	}
 
--- src/main/java/com/pauloricardo/projetoservidorubuntu/Services/ProductsService.java
+++ src/main/java/com/pauloricardo/projetoservidorubuntu/Services/ProductsService.java
@@ -1,4 +1,12 @@
 package com.pauloricardo.projetoservidorubuntu.Services;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Service
 public class ProductsService {
+
+
 }
