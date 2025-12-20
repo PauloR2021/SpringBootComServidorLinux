fae19ee5faceb1bbeacc7538d301494121bc8f96
PauloR2021 <paulotrindadeoficial@gmail.com>
2025-12-20 19:22:37 -0300

    Atualizando o Model para a criacao da base de dados

--- src/main/java/com/pauloricardo/projetoservidorubuntu/Services/ProductsService.java
+++ src/main/java/com/pauloricardo/projetoservidorubuntu/Services/ProductsService.java
@@ -4,6 +4,7 @@ import com.pauloricardo.projetoservidorubuntu.Dtos.ProductsRequestDTO;
 import com.pauloricardo.projetoservidorubuntu.Dtos.ProductsResponseDTO;
 import com.pauloricardo.projetoservidorubuntu.Models.ProductsModel;
 import com.pauloricardo.projetoservidorubuntu.Repositorys.ProductsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
@@ -11,23 +12,25 @@ import org.springframework.stereotype.Service;
 public class ProductsService {
 
     //Chamando a Classe de Repositorio
+    @Autowired
     private ProductsRepository productsRepository;
 
     //Criando o Método de Criar Produtos
     public ProductsResponseDTO create(ProductsRequestDTO dto){
+
         ProductsModel products = new ProductsModel();
         products.setName(dto.name());
         products.setQuantity(dto.quantity());
         products.setPrice(dto.price());
 
-        ProductsModel save = new ProductsModel();
+        ProductsModel save = productsRepository.save(products);
 
-        return new ProductsResponseDTO(
-                save.getId(),
-                save.getName(),
-                save.getQuantity(),
-                save.getPrice()
-        );
+       return new ProductsResponseDTO(
+               save.getId(),
+               save.getName(),
+               save.getQuantity(),
+               save.getPrice()
+       );
     }
 
 
