dc9d3e330fa245a2153a3d8c9c2a86e83b307531
PauloR2021 <paulotrindadeoficial@gmail.com>
2025-12-20 18:44:36 -0300

    Criando os comandos do Service

--- src/main/java/com/pauloricardo/projetoservidorubuntu/Services/ProductsService.java
+++ src/main/java/com/pauloricardo/projetoservidorubuntu/Services/ProductsService.java
@@ -1,12 +1,36 @@
 package com.pauloricardo.projetoservidorubuntu.Services;
 
-import org.springframework.http.ResponseEntity;
+import com.pauloricardo.projetoservidorubuntu.Dtos.ProductsRequestDTO;
+import com.pauloricardo.projetoservidorubuntu.Dtos.ProductsResponseDTO;
+import com.pauloricardo.projetoservidorubuntu.Models.ProductsModel;
+import com.pauloricardo.projetoservidorubuntu.Repositorys.ProductsRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Service
 public class ProductsService {
+
+    //Chamando a Classe de Repositorio
+    private ProductsRepository productsRepository;
+
+    //Criando o Método de Criar Produtos
+    public ProductsResponseDTO create(ProductsRequestDTO dto){
+        ProductsModel products = new ProductsModel();
+        products.setName(dto.name());
+        products.setQuantity(dto.quantity());
+        products.setPrice(dto.price());
+
+        ProductsModel save = new ProductsModel();
+
+        return new ProductsResponseDTO(
+                save.getId(),
+                save.getName(),
+                save.getQuantity(),
+                save.getPrice()
+        );
+    }
+
+
 
 
 }
