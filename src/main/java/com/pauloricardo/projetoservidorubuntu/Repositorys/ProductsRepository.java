e35de87cb7f3a4c8c6b6ede928485553b053dc5d
PauloR2021 <paulotrindadeoficial@gmail.com>
2025-12-20 18:43:36 -0300

    Modificando o Respository

--- src/main/java/com/pauloricardo/projetoservidorubuntu/Repositorys/ProductsRepository.java
+++ src/main/java/com/pauloricardo/projetoservidorubuntu/Repositorys/ProductsRepository.java
@@ -3,5 +3,5 @@ package com.pauloricardo.projetoservidorubuntu.Repositorys;
 import com.pauloricardo.projetoservidorubuntu.Models.ProductsModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductsReposiory extends JpaRepository<ProductsModel, Long> {
+public interface ProductsRepository extends JpaRepository<ProductsModel, Long> {
 }
